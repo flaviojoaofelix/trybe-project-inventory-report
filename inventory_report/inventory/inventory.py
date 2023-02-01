2455cba9d9b4edaaae7ff80811279bbfde87cd2b
Flávio João Félix <flaviojoaofelix.dev@gmail.com>
2023-02-01 16:40:58 -0300

    :sparkles: feat: req 05 - Gere os relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,31 @@
 from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    @staticmethod
-    def import_data(file_path: str, type: str):
+    @classmethod
+    def import_data(cls, file_path: str, report_type: str):
         if file_path.endswith(".csv"):
             data_list = CsvImporter.import_data(file_path)
 
-        return (
-            CompleteReport.generate(data_list)
-            if type == "completo"
-            else SimpleReport.generate(data_list)
-        )
+        elif file_path.endswith(".json"):
+            data_list = JsonImporter.import_data(file_path)
+
+        else:
+            raise ValueError("O formato do arquivo é desconhecido")
+
+        return cls.get_report(data_list, report_type)
+
+    def get_report(data: list, report_type: str):
+        if report_type == "completo":
+            report = CompleteReport.generate(data)
+
+        elif report_type == "simples":
+            report = SimpleReport.generate(data)
+
+        else:
+            raise ValueError("Tipo de relatório desconhecido")
+
+        return report
